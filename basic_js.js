591e47fa189182bf8e42b033f7211a78fcdd485b
Suraj2210-bit <141450781+Suraj2210-bit@users.noreply.github.com>
2023-10-25 18:53:59 +0000

    contener confi done

---
+++ basic_js.js
@@ -0,0 +1,13 @@
+// console.log("hello");
+// console.log("suraj.k");
+// console.log("HI! MY FIRST JAVASCRIPT PROGRAM");
+
+function avg(a,b){
+    return (a+b)/2;
+    
+    }
+    c1=avg(46,56)
+    c2=avg(5,6)
+    console.log(c2);
+    console.log(c1);
+    
